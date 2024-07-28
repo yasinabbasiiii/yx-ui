b619335c06a2e973110b5199759cbc7902b51bc8
Yasin Abbasi <engineer_yasin@yahoo.com>
2024-07-28 16:22:29 +0330

    1

---
+++ xray/client_traffic_details.go
@@ -0,0 +1,10 @@
+package xray
+
+type ClientTrafficDetails struct {
+	Id     int    `json:"id" form:"id" gorm:"primaryKey;autoIncrement"`
+	Enable bool   `json:"enable" form:"enable"`
+	Email  string `json:"email" form:"email" gorm:"unique"`
+	Up     int64  `json:"up" form:"up"`
+	Down   int64  `json:"down" form:"down"`
+	Total  int64  `json:"total" form:"total"`
+}
